3ca916c31652ea5f7cf8e34c81710dd87047d37c
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-18 20:19:49 +0530

    Exchange Section

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import axios from "axios";
+import { React, useEffect, useState } from "react";
+import { server } from "../index";
+import { Container, HStack } from "@chakra-ui/react";
+import Loader from "./Loader";
 
 const Exchange = () => {
-  return <div></div>;
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      const { data } = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+      setLoading(false);
+    };
+    fetchExchanges();
+  }, []);
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+const ExchangeCard = ({ name, img, rank, url }) => {
+  <a></a>;
 };
 
 export default Exchange;

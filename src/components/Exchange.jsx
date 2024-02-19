4093be07a8c8e3a7b67cc612827deecb63322b16
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-19 18:33:26 +0530

    Exchange Section

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,20 +1,38 @@
 import axios from "axios";
 import { React, useEffect, useState } from "react";
 import { server } from "../index";
-import { Container, HStack } from "@chakra-ui/react";
+import {
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Loader from "./Loader";
+import Error from "./Error";
 
 const Exchange = () => {
   const [exchanges, setExchanges] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchExchanges = async () => {
-      const { data } = await axios.get(`${server}/exchanges`);
-      setExchanges(data);
-      setLoading(false);
+      try {
+        const { data } = await axios.get(`${server}/exhanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
     };
     fetchExchanges();
   }, []);
+
+  if (error) {
+    return <Error message={"Error while fetching data"} />;
+  }
 
   return (
     <Container maxW={"container.xl"}>
@@ -40,7 +58,35 @@ const Exchange = () => {
 };
 
 const ExchangeCard = ({ name, img, rank, url }) => {
-  <a></a>;
+  return (
+    <a href={url} target={"blank"}>
+      <VStack
+        w={"40"}
+        shadow={"lg"}
+        p={"8"}
+        borderRadius={"lg"}
+        transition={"all 0.3s"}
+        m={"4"}
+        css={{
+          "&:hover": {
+            transform: "scale(1.1)",
+          },
+        }}
+      >
+        <Image
+          src={img}
+          w={"10"}
+          h={"10"}
+          objectFit={"contain"}
+          alt={"Exchange"}
+        />
+        <Heading size={"md"} noOfLines={1}>
+          {rank}
+        </Heading>
+        <Text noOfLines={1}>{name}</Text>
+      </VStack>
+    </a>
+  );
 };
 
 export default Exchange;

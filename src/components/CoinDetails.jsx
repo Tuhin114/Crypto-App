6a2fe610037b7f458ba16b420579357b417e0594
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-22 20:05:40 +0530

    Coin Section

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import {
+  Box,
+  Container,
+  HStack,
+  Radio,
+  RadioGroup,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { React, useState, useEffect } from "react";
+import Loader from "./Loader";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { server } from "../index";
+import Error from "./Error";
 
 const CoinDetails = () => {
-  return <div></div>;
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
+        setCoin(data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [params.id]);
+
+  if (error) {
+    return <Error message={"Error  Fetching Data"} />;
+  }
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}>
+            Box
+          </Box>
+
+          {/*Button*/}
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>EUR</Radio>
+              <Radio value={"eur"}>USD</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
+              Last Updated on {Date().split("G")[0]}
+            </Text>
+          </VStack>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default CoinDetails;

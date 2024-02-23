2e60909cc57bfc7b95167ad5cb049c7ab75400fe
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-23 20:35:59 +0530

    Coin Details

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,18 @@
 import {
+  Badge,
   Box,
   Container,
+  GridItem,
   HStack,
+  Image,
+  Progress,
   Radio,
   RadioGroup,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -19,6 +28,8 @@ const CoinDetails = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [currency, setCurrency] = useState("inr");
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
 
   const params = useParams();
 
@@ -62,13 +73,63 @@ const CoinDetails = () => {
 
           <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
             <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
-              Last Updated on {Date().split("G")[0]}
+              Last Updated on{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
             </Text>
+
+            <Image
+              src={coin.image.large}
+              w={"16"}
+              h={"16"}
+              objectFit={"contain"}
+            />
+
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatNumber>
+                {currencySymbol}
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
+                />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+            <Badge
+              fontSize={"2xl"}
+              bgColor={"blackAlpha.800"}
+              color={"white"}
+            >{`#${coin.market_cap_rank}`}</Badge>
+
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+            <Box w={"full"} p={"4"}>
+              <Item title={"Max Supply"} value={23231} />
+            </Box>
           </VStack>
         </>
       )}
     </Container>
   );
 };
+
+const CustomBar = ({ high, low }) => (
+  <VStack w={"full"}>
+    <Progress value={50} colorScheme="teal" w={"full"} />
+    <HStack justifyContent={"space-between"} w={"full"}>
+      <Badge children={low} colorScheme="red" />
+      <Text fontSize={"sm"}>24H Range</Text>
+      <Badge children={high} colorScheme="green" />
+    </HStack>
+  </VStack>
+);
 
 export default CoinDetails;

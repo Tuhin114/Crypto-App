adbc60b124164ca3ef6ddc7e891fa2ca0bb9f4c2
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-28 17:35:08 +0530

    Animate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,24 @@
 import { Box, Text, Image } from "@chakra-ui/react";
 import React from "react";
 import btcSrc from "../assets/btc.png";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
-      <Image
-        w={"full"}
-        h={"full"}
-        objectFit={"contain"}
-        src={btcSrc}
-        filter={"grayscale(1"}
-      />
+      <motion.div
+        style={{ height: "80vh" }}
+        animate={{ translateY: "20px" }}
+        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
+      >
+        <Image
+          w={"full"}
+          h={"full"}
+          objectFit={"contain"}
+          src={btcSrc}
+          filter={"grayscale(1"}
+        />
+      </motion.div>
       <Text
         fontSize={"6xl"}
         textAlign={"center"}
